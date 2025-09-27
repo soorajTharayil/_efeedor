05caaa7fc79200d8f3b547d94db627a97f73f36d
Mathews Mittathani <mathewsmittathani@gmail.com>
2025-09-27 17:10:29 +0530

    get_user file changed to php8

--- new_messages/get_user.php
+++ new_messages/get_user.php
@@ -1,91 +1,105 @@
 <?php 
 
-
-function get_user_by_sms_activity($type,$con){
+function get_user_by_sms_activity($type, $con) {
     $user_list = array();
-     $query = "SELECT * FROM features where feature_name='".$type."'";
-    $feature_result =  mysqli_fetch_object(mysqli_query($con, $query));
-    if (!$feature_result) {
-        //echo "Error: " . mysqli_error($con);
+
+    // Secure query
+    $type = mysqli_real_escape_string($con, $type);
+    $query = "SELECT feature_id FROM features WHERE feature_name='" . $type . "' LIMIT 1";
+    $result = mysqli_query($con, $query);
+
+    if (!$result || mysqli_num_rows($result) === 0) {
+        return $user_list; // No feature found
     }
-    $feauture = $feature_result->feature_id;
-     $user_permission_query = 'SELECT * FROM `user_permissions` where status = 1 AND `feature_id` ='.$feauture;
-    $permission_result =  mysqli_query($con, $user_permission_query);
-    while($user_permission = mysqli_fetch_object($permission_result)){
-       $user_id = $user_permission->user_id;
-         $user_query = 'SELECT * FROM `user` where`user_id` ='.$user_id;
-        $user_result =  mysqli_query($con, $user_query);
-        while($user = mysqli_fetch_object($user_result)){
-            $user_list[] = $user;
-            
+
+    $feature_result = mysqli_fetch_object($result);
+    $feature_id = (int) $feature_result->feature_id;
+
+    $user_permission_query = "SELECT user_id FROM user_permissions WHERE status=1 AND feature_id=" . $feature_id;
+    $permission_result = mysqli_query($con, $user_permission_query);
+
+    if ($permission_result) {
+        while ($user_permission = mysqli_fetch_object($permission_result)) {
+            $user_id = (int) $user_permission->user_id;
+
+            $user_query = "SELECT * FROM user WHERE user_id=" . $user_id;
+            $user_result = mysqli_query($con, $user_query);
+
+            if ($user_result) {
+                while ($user = mysqli_fetch_object($user_result)) {
+                    $user_list[] = $user;
+                }
+            }
         }
     }
 
- 
-   
     return $user_list;
- 
 }
 
 
-function get_user_by_access($type,$con,$userId,$roleId){
-    $user_list = array();
-    $query = "SELECT * FROM features where feature_name='".$type."'";
-    $feature_result =  mysqli_fetch_object(mysqli_query($con, $query));
-    if (!$feature_result) {
-        //echo "Error: " . mysqli_error($con);
-    }
-    $feature = $feature_result->feature_id;
-    $user_permission_query = 'SELECT * FROM `user_permissions` where user_id="'.$userId.'" AND `feature_id` ='.$feature;
-    $permission_result =  mysqli_query($con, $user_permission_query);
-    //print_r($permission_result); exit;
-    if($permission_result->num_rows == 0){
-        $user_permission_query = 'SELECT * FROM `role_permissions` where `role_id` = '.$roleId.' AND `feature_id` ='.$feature;
-        $permission_result =  mysqli_query($con, $user_permission_query);
-    }
-    
+function get_user_by_access($type, $con, $userId, $roleId) {
+    $type = mysqli_real_escape_string($con, $type);
+    $userId = mysqli_real_escape_string($con, $userId);
+    $roleId = (int) $roleId;
 
-    
-   
-    $role_permission = mysqli_fetch_object($permission_result);
-    if($role_permission->status == 1){
-        return true;
-    }else{
-        return false;
+    $query = "SELECT feature_id FROM features WHERE feature_name='" . $type . "' LIMIT 1";
+    $result = mysqli_query($con, $query);
+
+    if (!$result || mysqli_num_rows($result) === 0) {
+        return false; // No feature found
     }
-   
- 
+
+    $feature_result = mysqli_fetch_object($result);
+    $feature_id = (int) $feature_result->feature_id;
+
+    // First check user_permissions
+    $user_permission_query = "SELECT status FROM user_permissions WHERE user_id='" . $userId . "' AND feature_id=" . $feature_id . " LIMIT 1";
+    $permission_result = mysqli_query($con, $user_permission_query);
+
+    if (!$permission_result || mysqli_num_rows($permission_result) === 0) {
+        // If no user permission, check role_permissions
+        $role_permission_query = "SELECT status FROM role_permissions WHERE role_id=" . $roleId . " AND feature_id=" . $feature_id . " LIMIT 1";
+        $permission_result = mysqli_query($con, $role_permission_query);
+    }
+
+    if ($permission_result && mysqli_num_rows($permission_result) > 0) {
+        $role_permission = mysqli_fetch_object($permission_result);
+        return ($role_permission && (int) $role_permission->status === 1);
+    }
+
+    return false;
 }
 
 
-
-function get_user_by_question($question_key,$con){
-   
-    $user_list = array();
+function get_user_by_question($question_key, $con) {
     $final_user_list = array();
-    $query = "SELECT * FROM user where 1";
-    $users_result =  mysqli_query($con, $query);
+
+    $query = "SELECT * FROM user";
+    $users_result = mysqli_query($con, $query);
+
     if (!$users_result) {
-        //echo "Error: " . mysqli_error($con);
+        return $final_user_list; // No users
     }
+
     while ($user = mysqli_fetch_object($users_result)) {
         $department = json_decode($user->department);
-        
-        foreach($department as $row){
-            foreach($row as $r){
-              $keyvalue = explode(',',$r);
-              foreach($keyvalue as $k){
-                if($k === $question_key){
-                    $final_user_list[] = $user;
-                }
-              }
-            }
 
+        if (is_array($department) || is_object($department)) {
+            foreach ($department as $row) {
+                if (is_array($row) || is_object($row)) {
+                    foreach ($row as $r) {
+                        $keyvalue = explode(',', (string)$r);
+                        foreach ($keyvalue as $k) {
+                            if (trim($k) === $question_key) {
+                                $final_user_list[] = $user;
+                                break 3; // Found a match, skip to next user
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
+
     return $final_user_list;
- 
 }
-
-
- 
