8b48d5db1a80517e1a712a0dbd63f88ebbfbb42d
SOORAJ TS <121349752+soorajTharayil@users.noreply.github.com>
2025-11-19 12:26:03 +0530

    Update env.php

--- env.php
+++ env.php
@@ -1,10 +1,10 @@
 <?php
 $config_set = array(
   "DBHOST" => "localhost",
-  "BASE_URL" => "http://172.20.253.248/",
-  "DOMAIN" => "172.20.253.248",
-  "DBUSER" => "phpmyadmin",
-  "DBPASSWORD" => "Efeedor#@100",
+  "BASE_URL" => "",
+  "DOMAIN" => "",
+  "DBUSER" => "",
+  "DBPASSWORD" => "",
   //  "DBNAME" => "efeedor_usermanagement_speedissue",
   "DBNAME" => "efeedor_qms_new",
 
